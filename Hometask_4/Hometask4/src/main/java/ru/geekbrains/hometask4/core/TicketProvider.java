028f8b8062a51208d19db78bf0dc91b5ebd92257
Artem <kardan97@yandex.ru>
2023-09-12 22:03:01 +0700

    Some code editions

--- Hometask_4/Hometask4/src/main/java/ru/geekbrains/hometask4/core/TicketProvider.java
+++ Hometask_4/Hometask4/src/main/java/ru/geekbrains/hometask4/core/TicketProvider.java
@@ -27,10 +27,34 @@ public class TicketProvider {
 
     public boolean buyTicket(int clientId, String cardNo) {
 
+        // ПРЕДУСЛОВИЕ
+        if (clientId < 1) {
+            throw new RuntimeException("Ошибка! Передан неверный id клиента");
+        }
+        if (cardNo.length() != 16) {
+            throw new RuntimeException("Ошибка! Передан неверный номер карты клиента");
+        }
+
+        //РАБОТА С ДАННЫМИ
         int orderId = database.createTicketOrder(clientId);
         double amount = database.getTicketAmount();
-        return paymentProvider.buyTicket(orderId,  cardNo, amount);
+        // ИНВАРИАНТ
+        // validateAmount(amount);
 
+        // ИНВАРИАНТ
+        // validateAmount(amount);
+
+        // ПОСТУСЛОВИЕ
+        // проверяем, прошла ли транзакция
+        if (!paymentProvider.buyTicket(orderId,  cardNo, amount)) {
+            throw new RuntimeException("Ошибка обработки данных");
+            // ИНВАРИАНТ
+            // если оплата не прошла откатываем назад транзацию и билет снова доступен к покупке
+            // paymentProvider.rollback(orderId,  cardNo, amount);
+        }
+
+        //TODO: Возвращаем результат выполнения задачи ...
+        return true;
     }
 
     public boolean checkTicket(String qrcode) {
