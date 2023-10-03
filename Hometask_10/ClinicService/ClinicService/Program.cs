b9952ac43d6abd184b39f380af224f30c9f75d12
Artem <kardan97@yandex.ru>
2023-10-03 22:50:22 +0700

    Consultation repository added

--- Hometask_10/ClinicService/ClinicService/Program.cs
+++ Hometask_10/ClinicService/ClinicService/Program.cs
@@ -15,6 +15,7 @@ namespace ClinicService
             // Add services to the container.
             builder.Services.AddScoped<IClientRepository, ClientRepository>();
             builder.Services.AddScoped<IPetRepository, PetRepository>();
+            builder.Services.AddScoped<IConsultationRepository, ConsultationRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
